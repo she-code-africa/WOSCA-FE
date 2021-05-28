4f181c38d7048091231c6ef6d34361d13cf6887a
maryam-olabisi <m.olabisimurit@gmail.com>
2021-05-28 15:43:57 +0100

    FAQ Page, Footer, Header and Subscribe COmponents

---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Logo from "../assets/images/she-code-africa-logo.svg";
+import '../styles/components/header.css';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: true,
+        };
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <nav className="navbar navbar-expand-md navbar-lighter bg-lighter">
+                    <a className="navbar-brand" href="#">
+                        <img src={Logo} alt="Logo" className="logo" />
+                    </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Resources</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Events</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">FAQs</a>
+                            </li>
+
+                        </ul>
+
+                        <a className="nav-link" href="#">Sign In</a>
+                        <button className="btn btn-outline my-2 my-sm-0">GET STARTED</button>
+                    </div>
+                </nav>
+            </React.Fragment>
+        )
+    }
+}
+export default Header;
---
+++ src/components/subscribe.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import '../styles/components/subscribe.css';
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+class Subscribe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: true,
+        };
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col"></div>
+                <div className="col-md-10">
+                    <div className="jumbotrons">
+                        <div className="row">
+                            <div className="col"></div>
+                            <div className="col-md-7">
+                                <h2>Subscribe to our Newsletter</h2>
+                                <p className="leads"> Support open source and pick a limited edition T-shirt or plant a tree.</p>
+                                <Form>
+                                    <Form.Row>
+                                        <Col md={8}>
+                                            <Form.Control placeholder="Email Address" />
+                                        </Col>
+                                        <Col >
+                                            <Button className="sub-button" type="submit">SUBSCRIBE</Button>
+                                        </Col>
+                                    </Form.Row>
+                                </Form>
+                            </div>
+                            <div className="col"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col"></div>
+            </div>
+
+        )
+    }
+}
+export default Subscribe;
