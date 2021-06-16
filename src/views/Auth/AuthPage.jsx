9745dcd14798a3c3597f1bf1adf8a3ff794e8c3c
EseAlli <essealli.ea@gmail.com>
2021-06-16 17:47:33 +0100

    Style cleanup to fix responsiveness issue

--- src/views/Auth/AuthPage.jsx
+++ src/views/Auth/AuthPage.jsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/images/she-code-africa-logo.svg";
 import LoginForm from "./LoginForm";
 import SignUpForm from "./SignUpForm";
 
-function AuthPage({children}) {
+function AuthPage({ children }) {
   const [activity, setActivity] = useState(false);
 
   const toggleActivity = () => {
@@ -12,22 +12,24 @@ function AuthPage({children}) {
   };
   return (
     <div className="main-container">
-      <div className="logo-box">
-        <img src={Logo} alt="Logo" className="logo" />
-      </div>
-      <div className="text-box">
-        <h1 className="heading-primary">
-          <span className="heading-primary-main">
-            Start contributing to open source project
-          </span>
-        </h1>
-        <p className="heading-primary-sub">
-          Create a free account to get started on your journey to world-known
-          contributor
-        </p>
-      </div>
-      <div className="forms">
-        {children}
+      <div className="inner-container">
+        <div className="logo-box">
+          <img src={Logo} alt="Logo" className="logo" />
+        </div>
+        <div className="main-body">
+          <div className="text-box">
+            <h1 className="heading-primary">
+              <span className="heading-primary-main">
+                Start contributing to open source project
+              </span>
+            </h1>
+            <p className="heading-primary-sub">
+              Create a free account to get started on your journey to
+              world-known contributor
+            </p>
+          </div>
+          <div className="forms">{children}</div>
+        </div>
       </div>
     </div>
   );
