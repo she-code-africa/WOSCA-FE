8bbbb106306b9a91b55cc615f0b84331c07073d9
EseAlli <essealli.ea@gmail.com>
2021-07-27 21:45:48 +0100

    Added button to card and more responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-
-export function Card({ title, icon, subtitle, details, value }) {
+export function Card({ title, icon, subtitle, details, value, imgurl }) {
 
   return (
     <div className="card-box">
+      <div className="card-content">
       <div className="card-title tr">
+        <div className="icon-cardbox">
+          {icon}
+        </div>
         <p className="value">{value}</p>
       </div>
-      <p className="card-title">{title}</p>
+      <p className="title">{title}</p>
+      </div>
     </div>
   );
 }
 
-export function BigCard({ title, subtitle }) {
+export function BigCard({ title, subtitle, button }) {
   return (
     <div className="card-box-lg withOverflowingBackground">
       <div className="card-title tr">
         <h6 className="lx-s"> {title}</h6>
         <p>{subtitle}</p>
       </div>
-      <p className="lm">5 Tonnes</p>
+      {button}
     </div>
   );
 }
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import Logo from "../../assets/images/she-code-africa-logo.svg";
 import {Card, BigCard} from "../../components/Card";
 import Table from "../../components/Table"
+import {AiOutlineLink} from 'react-icons/ai'
+import {BsCheckAll, BsArrowRight} from 'react-icons/bs'
 
-function Dashboard({  }) {
+function Dashboard() {
   const colunms = ["Date", "Contribution Link", "Status"]
     return (
       <div className="dashboard">
@@ -14,11 +16,11 @@ function Dashboard({  }) {
           <div className="dashboard-body">
            <div className="greeting"> <p className="greeting-text">Good Morning, Lola</p></div>
            <div className="contribution-cards">
-            <BigCard title="New" subtitle="Contribution"/>
+            <BigCard title="New" subtitle="Contributions" button={<button>Submit New Pull Request <div className="card-btn-icon"><BsArrowRight/></div></button>}/>
 
             {/* <BigCard/> */}
-            <Card title="Total Contributions" value="345"/>
-            <Card title="Accepted Contributions" value="345"/>
+            <Card title="Total Contributions" value="345" icon={<AiOutlineLink color={'#fff'} />}/>
+            <Card title="Accepted Contributions" value="345" icon={<BsCheckAll color={'#fff'} />}/>
            </div>
            <div className="greeting">
             <p className="greeting-text">Your Submissions</p>
