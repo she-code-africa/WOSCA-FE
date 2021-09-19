a74f9dd819ac22c555f24adc1f03bcfac395721f
Elizabeth Damilola <leezeelola@gmail.com>
2021-09-19 22:42:52 +0100

    update auth

--- src/App.js
+++ src/App.js
@@ -10,40 +10,45 @@ import Resources from "../src/views/resources/resources";
 import ForgotPassword from "./views/Auth/ForgotPassword"
 import ResetPassword from "./views/Auth/ResetPassword"
 
+import ProtectedRoute from './components/routes/ProtectedRoute';
+
+import { UserProvider } from "./context/AuthContext"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact
-          path="/signup"
-          name="SignUpForm"
-          component={SignUpForm} 
-        />
-        <Route exact
-          path="/signin"
-          name="LoginForm"
-          component={LoginForm}
-        />
-        <Route exact
-          path ="/dashboard"
-          component={Dashboard}
-        />
-        <Route exact
-          path ="/forgot-password"
-          component={ForgotPassword}
-        />
-        <Route exact
-          path ="/reset-password"
-          component={ResetPassword}
-        />
-        <Route path="/faq"> <FAQ /> </Route>
-        <Route path="/resources"> <Resources /> </Route>
-        <Route path="/events"> <Events /> </Route>
-        <Route path="/"> <Home /> </Route>
+    <UserProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact
+            path="/signup"
+            name="SignUpForm"
+            component={SignUpForm} 
+          />
+          <Route exact
+            path="/signin"
+            name="LoginForm"
+            component={LoginForm}
+          />
+          <ProtectedRoute exact
+            path ="/dashboard"
+            component={Dashboard}
+          />
+          <Route exact
+            path ="/forgot-password"
+            component={ForgotPassword}
+          />
+          <Route exact
+            path ="/reset-password"
+            component={ResetPassword}
+          />
+          <Route path="/faq"> <FAQ /> </Route>
+          <Route path="/resources"> <Resources /> </Route>
+          <Route path="/events"> <Events /> </Route>
+          <Route path="/"> <Home /> </Route>
 
-      </Switch>
-    </BrowserRouter>
+        </Switch>
+      </BrowserRouter>
+    </UserProvider>
   );
 }
 
